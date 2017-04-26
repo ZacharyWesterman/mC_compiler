0c2adc3faa62e03880c6f79b195abfde4a733183
Unknown <zacharywesterman@yahoo.com>
2017-04-26 18:11:27 -0500

    added some more instruction types

--- asm_instruction.h
+++ asm_instruction.h
@@ -3,9 +3,19 @@
 
 #define ASM_MAX_PARAMS 3
 
+#define GEN_REG_COUNT 13
+
+
 //enumerate ARM instruction types
 enum
 {
+  DATA_SEG,
+  PROGRAM_SEG,
+  END_PROGRAM,
+
+  ALIGN_BYTES,  
+  GLOBAL_DEF,
+
   MOVE_REG,
   WRITE_MEM,
   READ_MEM,
