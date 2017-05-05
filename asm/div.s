1006eb62dfaf80a527349ccc6ccb6c0cb7179bb8
Unknown <zacharywesterman@yahoo.com>
2017-05-05 18:35:41 -0500

    added helping comments

--- asm/div.s
+++ asm/div.s
@@ -22,8 +22,8 @@ _int_output:
 	push	{r0, r1, r2, r3, r4, r5, r7, lr}
 
 	cmp	r0, #0
-	bgt	.iout_not_negative
-	blt	.iout_is_negative
+	bgt	.iout_not_negative @l_1
+	blt	.iout_is_negative @l_0
 
 	@ print "0\n"
 	mov	r0, #48
@@ -38,9 +38,9 @@ _int_output:
 
 	swi	0
 
-	b	.iout_print_exit
+	b	.iout_print_exit @l_5
 
-	.iout_is_negative:
+	.iout_is_negative: @l_0
 	mov	r3, r0
 
 	@ number is negative, print "-"
@@ -57,15 +57,15 @@ _int_output:
 
 	sub	r0, #0, r3
 
-	.iout_not_negative:
+	.iout_not_negative: @l_1
 
 	mov	r4, #0
 
 	sub	r3, sp, #30	@ buffer must be AT LEAST 20 chars below the stack(sp)
 
-	b	.ioutconv_test
+	b	.ioutconv_test @l_3
 
-	.ioutconv_loop:
+	.ioutconv_loop: @l_2
 		mov	r1, #10	@ ints are at most 10 digits long
 		bl	_int_div
 
@@ -76,9 +76,9 @@ _int_output:
 		add	r3, r3, #1
 		add	r4, r4, #1
 
-	.ioutconv_test:
+	.ioutconv_test: @l_3
 	cmp	r0, #0
-	bgt	.ioutconv_loop
+	bgt	.ioutconv_loop @l_2
 
 
 	
@@ -87,7 +87,7 @@ _int_output:
 
 	add	r7, r3, r4
 
-	.ioutinvert_loop:
+	.ioutinvert_loop: @l_4
 		ldr	r2, [r0]
 		str	r2, [r1]
 
@@ -95,7 +95,7 @@ _int_output:
 		sub	r0, r0, #1
 
 	cmp	r7, r1
-	bgt	.ioutinvert_loop
+	bgt	.ioutinvert_loop @l_4
 
 	@append a newline character to the end
 	add	r4, r4, #1
@@ -109,7 +109,7 @@ _int_output:
 	mov	r1, r3		@ call to print
 	swi	0
 
-	.iout_print_exit:
+	.iout_print_exit: @l_5
 
 	pop	{r0, r1, r2, r3, r4, r5, r7, pc}	@ exit function
 
