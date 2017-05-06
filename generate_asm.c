4617387e098f676baf105302eff588644cae0fd8
Unknown <zacharywesterman@yahoo.com>
2017-05-06 02:43:26 -0500

    code gen: int literals are loaded to R0.

--- generate_asm.c
+++ generate_asm.c
@@ -1,6 +1,30 @@
 #include "generate_asm.h"
 
+
 void generate_asm(tree* ast)
 {
+  if (ast)
+  {
+    if (ast->nodeKind == INTEGER)
+    {
+      if ((ast->val) > LITERAL_MAX)
+      {
+        add_data(LABEL, CONST_LABEL, ast->val, 0);
+        add_data(WORD, ast->val, 0, 0);
 
+        add_instr(LDR, R0, CONST_LABEL, ast->val);
+        add_instr(LDR, R0, R0, 0);
+      }
+      else
+      {
+        add_instr(MOV, R0, ast->val, 0);
+      }
+    }
+    else
+    {
+      int i;
+      for (i=0; i<(ast->numChildren); i++)
+        generate_asm(ast->children[i]);
+    }
+  }
 }
