685a3291fba584d6884e5fce3f2e21f9844c3649
Unknown <zacharywesterman@yahoo.com>
2017-05-06 03:15:41 -0500

    code gen: function entry and exit are working.

--- generate_asm.c
+++ generate_asm.c
@@ -5,6 +5,7 @@ void generate_asm(tree* ast)
 {
   if (ast)
   {
+    //Integer literals are loaded to R0
     if (ast->nodeKind == INTEGER)
     {
       if ((ast->val) > LITERAL_MAX)
@@ -19,6 +20,25 @@ void generate_asm(tree* ast)
       {
         add_instr(MOV, R0, ast->val, 0);
       }
+    }
+    //Functions save all registers except R0 and R1.
+    //The stack should be 8-byte aligned at all times.
+    else if (ast->nodeKind == FUNCDECL)
+    {
+      add_instr(LABEL, ast->children[1]->val, 0, 0);
+      
+      int i;
+      add_instr(PUSH, R2, LR, 0); 
+
+      for (i=R3; i<R12; i+=2)
+        add_instr(PUSH, i, i+1, 0);         
+
+      generate_asm(ast->children[2]);
+
+      for (i=R12; i>=R3; i-=2)
+        add_instr(POP, i-1, i, 0); 
+
+      add_instr(POP, R2, PC, 0); 
     }
     else
     {
