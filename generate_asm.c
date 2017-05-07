3d5b7bdd834a0435de229019bfd860d936b0424f
Unknown <zacharywesterman@yahoo.com>
2017-05-06 19:42:09 -0500

    code gen: added relational operators

--- generate_asm.c
+++ generate_asm.c
@@ -1,9 +1,23 @@
 #include "generate_asm.h"
 
 #define OPER_ADD 266
+#define OPER_SUB 267
 #define OPER_MUL 268
+#define OPER_DIV 269
+
+#define OPER_LT  270
+#define OPER_GT  271
+#define OPER_LTE 272
+#define OPER_GTE 273
+#define OPER_EQ  274
+#define OPER_NEQ 275
+
+#define FUNC_INPUT -1
 
 int scope;
+
+int stack_offs[1000];
+int curr_stack_offs;
 
 void generate_asm(tree* ast)
 {
@@ -15,7 +29,8 @@ void generate_asm(tree* ast)
       //if the variable is local
       if (scope)
       {
-
+        curr_stack_offs -= 4;
+        stack_offs[ast->children[1]->val] = curr_stack_offs;
       }
       //if the variable is global
       else
@@ -23,6 +38,59 @@ void generate_asm(tree* ast)
         add_data(LABEL, DATA_LABEL, ast->children[1]->val, 0);
         add_data(WORD, 0, 0, 0);
       }
+    }
+    //accessing a variable (assumed to be a single integer)
+    else if (ast->nodeKind == VAR)
+    {
+      //if the variable is local
+      if (scope)
+      {
+        int offs = stack_offs[ast->children[0]->val];
+
+        if (offs == FUNC_INPUT)
+          add_instr(MOV, R1, R3, 0);
+        else
+          add_instr(LDR, R1, SP, offs);
+      }
+      //if the variable is global
+      else
+      {
+        add_instr(LDR, R1, DATA_LABEL, ast->children[0]->val);
+        add_instr(LDR, R1, R1, 0);
+      }
+    }
+    //assume function input is a single integer, stored in R0.
+    else if (ast->nodeKind == FORMALDECL)
+    {
+      add_instr(MOV, R3, R0, 0);
+      
+      stack_offs[ast->children[1]->val] = FUNC_INPUT;
+    }
+    //calling a function. 
+    //Function input is a single integer, stored in R0.
+    //Function return value is in R0.
+    else if (ast->nodeKind == FUNCCALL)
+    {
+      if (ast->numChildren > 0)
+      {
+        generate_asm(ast->children[1]);
+        add_instr(MOV, R0, R1, 0);
+      }
+
+      add_instr(BL, ast->children[0]->val, 0,0);
+      add_instr(MOV, R1, R0, 0);
+    }
+    //if we are assigning a value to a variable
+    else if (ast->nodeKind == ASSIGN)
+    {
+      generate_asm(ast->children[1]);
+
+      int offs = stack_offs[ast->children[0]->children[0]->val];
+
+      if (offs == FUNC_INPUT)
+        add_instr(MOV, R3, R1, 0);
+      else
+        add_instr(STR, R1, SP, offs);
     }
     //Integer literals are loaded to R1
     else if (ast->nodeKind == INTEGER)
@@ -71,6 +139,50 @@ void generate_asm(tree* ast)
         add_instr(MOV, R1, R0, 0);
       }
     }
+    //relational operations evaluated on R1 (R2 modified)
+    else if (ast->nodeKind == EXPR)
+    {
+      generate_asm(ast->children[0]);
+      add_instr(MOV, R2, R1, 0);
+      generate_asm(ast->children[2]);
+
+      if (ast->children[1]->val == OPER_GT)
+      {
+        add_instr(CMP, R2, R1, 0);
+        add_instr(MOVGT, R1, 1, 0);
+        add_instr(MOVLE, R1, 0, 0);
+      }
+      else if (ast->children[1]->val == OPER_LT)
+      {
+        add_instr(CMP, R2, R1, 0);
+        add_instr(MOVLT, R1, 1, 0);
+        add_instr(MOVGE, R1, 0, 0);
+      }
+      else  if (ast->children[1]->val == OPER_GTE)
+      {
+        add_instr(CMP, R2, R1, 0);
+        add_instr(MOVGE, R1, 1, 0);
+        add_instr(MOVLT, R1, 0, 0);
+      }
+      else  if (ast->children[1]->val == OPER_LTE)
+      {
+        add_instr(CMP, R2, R1, 0);
+        add_instr(MOVLE, R1, 1, 0);
+        add_instr(MOVGT, R1, 0, 0);
+      }
+      else  if (ast->children[1]->val == OPER_EQ)
+      {
+        add_instr(CMP, R2, R1, 0);
+        add_instr(MOVEQ, R1, 1, 0);
+        add_instr(MOVNE, R1, 0, 0);
+      }
+      else //OPER_NEQ
+      {
+        add_instr(CMP, R2, R1, 0);
+        add_instr(MOVNE, R1, 1, 0);
+        add_instr(MOVEQ, R1, 0, 0);
+      }
+    }
     //Functions save all registers except R0 and R1.
     //The stack should be 8-byte aligned at all times.
     else if (ast->nodeKind == FUNCDECL)
@@ -83,8 +195,10 @@ void generate_asm(tree* ast)
       for (i=R3; i<R12; i+=2)
         add_instr(PUSH, i, i+1, 0);         
 
+      curr_stack_offs = 0;
       scope++;
       generate_asm(ast->children[2]);
+      generate_asm(ast->children[3]);
       scope--;
 
       for (i=R12; i>=R3; i-=2)
@@ -96,7 +210,7 @@ void generate_asm(tree* ast)
     //By default, statement "return;" returns 0.
     else if (ast->nodeKind == RETURN)
     {
-      if (ast->numChildren)
+      if (ast->numChildren > 0)
       {
         generate_asm(ast->children[0]);
         add_instr(MOV, R0, R1, 0);
